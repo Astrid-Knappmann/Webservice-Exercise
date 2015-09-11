ad2a5a6dd575f1f48ee3d524a3d35bebac3f83d1
Knappmann <7h380x@live.dk>
2015-09-11 08:33:21 +0200

    d

---
+++ src/webservice/exercise/WebserviceExercise.java
@@ -0,0 +1,17 @@
+
+package webservice.exercise;
+
+/**
+ *
+ * @author Patrick
+ */
+public class WebserviceExercise {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
