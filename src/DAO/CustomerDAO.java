92ea97cad26b1a84e434ea2eb37ec1fd8d1d4354
Astrinaar <7h380x@gmail.com>
2015-09-13 14:06:34 +0200

    getCustomer

--- src/DAO/CustomerDAO.java
+++ src/DAO/CustomerDAO.java
@@ -2,6 +2,7 @@
 package DAO;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
@@ -27,6 +28,18 @@ public class CustomerDAO {
         } catch (SQLException ex) {
             Logger.getLogger(CustomerDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+    
+    public Customer getCustomer(String id){
+        try {
+            Statement st = dbConn.createStatement();
+            ResultSet rs = st.executeQuery("SELECT * FROM customers WHERE CustomerID='" + id + "'");
+            rs.next();
+            return new Customer(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), rs.getString(7), rs.getString(8), rs.getString(9), rs.getString(10), rs.getString(11));
+        } catch (SQLException ex) {
+            Logger.getLogger(CustomerDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
     }
     
     
