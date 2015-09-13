8f93ac64b13fa63d59614ded3b188bf6bd775bf3
Astrinaar <7h380x@gmail.com>
2015-09-13 13:05:28 +0200

    Customer DAO Create

--- src/DAO/CustomerDAO.java
+++ src/DAO/CustomerDAO.java
@@ -2,6 +2,11 @@
 package DAO;
 
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Customer;
 
 /**
  *
@@ -13,6 +18,15 @@ public class CustomerDAO {
 
     public CustomerDAO(Connection dbConn) {
         this.dbConn = dbConn;
+    }
+    
+    public void createCustomer(Customer customer){
+        try {
+            Statement st = dbConn.createStatement();
+            st.execute("INSERT INTO customers (CustomerID, CompanyName, ContactName, ContactTitle, Address, City, Region, PostalCode, Country, Phone, Fax) VALUES ('" + customer.getId() + "', '" + customer.getCompanyName() + "', '" + customer.getContactName() + "', '" + customer.getContactTitle() + "', '" + customer.getAddress() + "', '" + customer.getCity() + "', '" + customer.getRegion() + "', '" + customer.getPostalCode() + "', '" + customer.getCountry() + "', '" + customer.getPhone() + "', '" + customer.getFax() + "')");
+        } catch (SQLException ex) {
+            Logger.getLogger(CustomerDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
     
